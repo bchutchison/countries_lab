eba8741e56e8e3cfcb6b726687f90c5f795c838b
bchutchison <benhutchison85@gmail.com>
2019-08-06 14:08:08 +0100

    total population

--- src/app.js
+++ src/app.js
@@ -4,11 +4,27 @@ document.addEventListener('DOMContentLoaded', () => {
 new Vue({
   el: "#app",
   data: {
-
+    countriesArray: []
   },
+  mounted() {
+    console.log("I am mounted");
+    this.fetchCountries();
+    console.log(this.countriesArray);
+  },
+  computed: {
+    totalPopulation: function() {
+      return this.countriesArray.reduce((sum, country) => {
+        return sum + country.population
+      },0);
+  },
+},
   methods: {
-
+    fetchCountries: function() {
+      const request = fetch("https://restcountries.eu/rest/v2/all")
+      .then(response => response.json())
+      .then(countries => this.countriesArray = countries)
+    }
   }
-  
+
 });
 });
